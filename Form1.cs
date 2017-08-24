d96a5ff30c13232a46ea83cfdfbb2f694c41b4f1
asigna <asigna@laburl>
2017-08-24 07:34:15 -0600

    agregado textbox

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace pruebaDeGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = textBox1.Text;
+        }
     }
 }
